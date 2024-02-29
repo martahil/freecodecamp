01699e757537613fb2f49c58ede4de41399c045a
Marta Hilgert <marta.hil.1988@gmail.com>
2024-02-29 12:04:28 -0300

    Music Player project included in the main index.html

--- script.js
+++ script.js
@@ -74,5 +74,42 @@ function expandirTexto2() {
     }
 }
 
+function expandirTexto3() {
+    var texto = document.getElementById('textoEscondido3');
+    var learnMoreTxt = document.getElementById('learnMoreTxt3')
+
+    if (texto.style.display === 'none' || texto.style.display === '') {
+        texto.style.display = 'block';
+
+        /*learnMoreTxt.style.backgroundColor = '#040404';
+        learnMoreTxt.style.borderRadius = '12px';
+        learnMoreTxt.style.padding = '12px';
+        learnMoreTxt.style.width = '220px';
+        learnMoreTxt.style.zIndex = '3';
+        learnMoreTxt.style.position = "relative";*/
+
+        Object.assign(learnMoreTxt.style, {
+            backgroundColor: '#040404',
+            borderRadius: '12px',
+            paddingRigth: '12px',
+            paddingLeft: '24px',
+            paddingTop: '10px',
+            paddingBottom: '10px',
+            width: '440px',
+            zIndex: '3',
+            position: 'relative'
+        });
+
+
+        if (document.getElementById("plusCircle3").src = "Images/plusCircle.png") {
+            document.getElementById("plusCircle3").src = "Images/minusCircle.png";
+        }
+    } else {
+        texto.style.display = 'none';
+        learnMoreTxt.style.backgroundColor = '#262626';
+
+        document.getElementById("plusCircle3").src = "Images/plusCircle.png";
+    }
+}
 
 
