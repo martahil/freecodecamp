fd9942669c5ea8374dc6149d27b4f8785e4b0a28
Marta Hilgert <marta.hil.1988@gmail.com>
2024-03-19 15:09:24 -0300

    Number Sorter and Statistics Calculator - ready and included in the main Index.html

--- script.js
+++ script.js
@@ -341,4 +341,75 @@ function expandirTexto9() {
     }
 }
 
+function expandirTexto10() {
+    var texto = document.getElementById('textoEscondido10');
+    var learnMoreTxt = document.getElementById('learnMoreTxt10')
+
+    if (texto.style.display === 'none' || texto.style.display === '') {
+        texto.style.display = 'block';
+
+        /*learnMoreTxt.style.backgroundColor = '#040404';
+        learnMoreTxt.style.borderRadius = '12px';
+        learnMoreTxt.style.padding = '12px';
+        learnMoreTxt.style.width = '220px';
+        learnMoreTxt.style.zIndex = '3';
+        learnMoreTxt.style.position = "relative";*/
+
+        Object.assign(learnMoreTxt.style, {
+            backgroundColor: '#040404',
+            borderRadius: '12px',
+            paddingRight: '24px',
+            paddingLeft: '24px',
+            paddingTop: '12px',
+            paddingBottom: '12px',
+            zIndex: '3',
+            position: 'relative'
+        });
+        
+        if (document.getElementById("plusCircle10").src = "Images/plusCircle.png") {
+            document.getElementById("plusCircle10").src = "Images/minusCircle.png";
+        }
+    } else {
+        texto.style.display = 'none';
+        learnMoreTxt.style.backgroundColor = '#262626';
+
+        document.getElementById("plusCircle10").src = "Images/plusCircle.png";
+    }
+}
+
+function expandirTexto11() {
+    var texto = document.getElementById('textoEscondido11');
+    var learnMoreTxt = document.getElementById('learnMoreTxt11')
+
+    if (texto.style.display === 'none' || texto.style.display === '') {
+        texto.style.display = 'block';
+
+        /*learnMoreTxt.style.backgroundColor = '#040404';
+        learnMoreTxt.style.borderRadius = '12px';
+        learnMoreTxt.style.padding = '12px';
+        learnMoreTxt.style.width = '220px';
+        learnMoreTxt.style.zIndex = '3';
+        learnMoreTxt.style.position = "relative";*/
+
+        Object.assign(learnMoreTxt.style, {
+            backgroundColor: '#040404',
+            borderRadius: '12px',
+            paddingRight: '24px',
+            paddingLeft: '24px',
+            paddingTop: '12px',
+            paddingBottom: '12px',
+            zIndex: '3',
+            position: 'relative'
+        });
+        
+        if (document.getElementById("plusCircle11").src = "Images/plusCircle.png") {
+            document.getElementById("plusCircle11").src = "Images/minusCircle.png";
+        }
+    } else {
+        texto.style.display = 'none';
+        learnMoreTxt.style.backgroundColor = '#262626';
+
+        document.getElementById("plusCircle11").src = "Images/plusCircle.png";
+    }
+}
 
